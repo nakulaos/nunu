e57afbc9b1cec4eaa3a42287652502cfd84ac2f2
nakulaos <2821195622@qq.com>
2024-02-02 23:57:06 +0800

    i18n 初始化配置

--- backend/i18n/i18n.go
+++ backend/i18n/i18n.go
@@ -9,3 +9,80 @@
  */
 
 package i18n
+
+import (
+	"embed"
+	ginI18n "github.com/gin-contrib/i18n"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/text/language"
+	"gopkg.in/yaml.v3"
+)
+
+//go:embed lang/*
+var fs embed.FS
+
+//
+//func GetMsgWithMap(key string, maps map[string]interface{}) string {
+//	content := ""
+//	if maps == nil {
+//		content = ginI18n.MustGetMessage(&i18n.LocalizeConfig{
+//			MessageID: key,
+//		})
+//	} else {
+//		content = ginI18n.MustGetMessage(&i18n.LocalizeConfig{
+//			MessageID:    key,
+//			TemplateData: maps,
+//		})
+//	}
+//
+//	content = strings.ReplaceAll(content, ": <no value>", "")
+//	if content == "" {
+//		return key
+//	} else {
+//		return content
+//	}
+//}
+//
+//func GetErrMsg(key string, maps map[string]interface{}) string {
+//	content := ""
+//	if maps == nil {
+//		content = ginI18n.MustGetMessage(&i18n.LocalizeConfig{
+//			MessageID: key,
+//		})
+//	} else {
+//		content = ginI18n.MustGetMessage(&i18n.LocalizeConfig{
+//			MessageID:    key,
+//			TemplateData: maps,
+//		})
+//	}
+//	return content
+//}
+//
+//func GetMsgByKey(key string) string {
+//	content := ginI18n.MustGetMessage(&i18n.LocalizeConfig{
+//		MessageID: key,
+//	})
+//	return content
+//}
+
+// GinI18nLocalize 应在初始化router时进行全局注册
+func GinI18nLocalize() gin.HandlerFunc {
+	return ginI18n.Localize(
+		ginI18n.WithBundle(&ginI18n.BundleCfg{
+			RootPath:         "./lang",
+			AcceptLanguage:   []language.Tag{language.Chinese, language.English, language.TraditionalChinese, language.German, language.French},
+			DefaultLanguage:  language.Chinese,
+			FormatBundleFile: "yaml",
+			UnmarshalFunc:    yaml.Unmarshal,
+			Loader:           &ginI18n.EmbedLoader{FS: fs},
+		}),
+		ginI18n.WithGetLngHandle(
+			func(context *gin.Context, defaultLng string) string {
+				lng := context.GetHeader("Accept-Language")
+				if lng == "" {
+					return defaultLng
+				}
+				return lng
+			},
+		))
+}
