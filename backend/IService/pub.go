74f62510c2c65fe244237c6ff9c97772da5d158b
nakulaos <2821195622@qq.com>
2024-02-24 23:10:07 +0800

    pub 用户名登录功能

---
+++ backend/IService/pub.go
@@ -0,0 +1,24 @@
+/**
+ ******************************************************************************
+ * @file           : pub.go
+ * @author         : nakulaos
+ * @brief          : None
+ * @attention      : None
+ * @date           : 2024/2/13
+ ******************************************************************************
+ */
+
+package IService
+
+import (
+	"nunu/backend/dto"
+	"nunu/backend/pkg/xerror"
+)
+
+type IPubService interface {
+	SendCaptchaWithMailReq(req *dto.SendCaptchaWithMailReq) xerror.Error
+	SendCaptchaWithPhoneReq(req *dto.SendCaptchaWithPhoneReq) xerror.Error
+	GetCaptcha() (*dto.GetCaptchaResp, xerror.Error)
+	Register(req *dto.RegisterReq) (*dto.RegisterResp, xerror.Error)
+	LoginWithUsername(req *dto.LoginReqWithUserName) (*dto.LoginResp, xerror.Error)
+}
